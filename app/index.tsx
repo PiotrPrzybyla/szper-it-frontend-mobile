aeaac1e3efd31d781d72d61f5bd632bf9a012cfc
PiotrPrzybyla <pepapek@gmail.com>
2024-02-27 19:54:37 +0100

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Szper It</Text>
+      <Text>Wyszperaj swój pokój</Text>
+    </View>
+  );
+}
+export default Home;
